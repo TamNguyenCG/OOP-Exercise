8ae7d9f2129fcb8cdc505f0a3357808e93c35f02
TamNguyenCG <ngductam91@gmail.com>
2021-04-22 15:20:20 +0700

    Di chuyển ảnh

---
+++ MovingObject/JS.js
@@ -0,0 +1,29 @@
+function Hero(image, top, left, size) {
+    this.image = image;
+    this.top = top;
+    this.left = left;
+    this.size = size;
+
+    this.getHeroElement = function(){
+        return '<img width="'+ this.size + '"' +
+            ' height="'+ this.size + '"' +
+            ' src="' + this.image +'"' +
+            ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
+    }
+    this.moveRight = function () {
+        this.left += 50;
+        console.log('ok: ' + this.left);
+    }
+}
+
+let hero = new Hero('TL.jpg', 50, 0, 500);
+
+function start() {
+    if (hero.left < window.innerWidth - hero.size) {
+        hero.moveRight();
+    }
+    document.getElementById('game').innerHTML = hero.getHeroElement();
+    setTimeout(start, 500)
+}
+
+start();
